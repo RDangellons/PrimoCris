95714f779de633cbdbbee5b7cbd138f28256f480
Angel alonso <98786236+RDangellons@users.noreply.github.com>
2025-10-06 15:54:32 -0600

    cambios

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Querias una carta pero estas con un ingeniero JAJAJA'.split('')
+const text = 'SOLO QUIERO RECORDARTE LO MUCHO QUE TE AMO'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
